31738e60b6c3b17e56929f4b19b86a0d1e4334f1
vhuseinova-msft <98852890+vhuseinova-msft@users.noreply.github.com>
2023-03-13 16:27:30 -0700

    Updated the demo app

--- ChatApp/src/App.tsx
+++ ChatApp/src/App.tsx
@@ -1,73 +1,112 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { AzureCommunicationTokenCredential, CommunicationUserIdentifier } from '@azure/communication-common';
-import {ChatComposite, MessageProps, MessageRenderer, useAzureCommunicationChatAdapter} from '@azure/communication-react';
+import React, { useEffect, useMemo, useState } from "react";
+import "./App.css";
+import {
+  AzureCommunicationTokenCredential,
+  CommunicationUserIdentifier,
+} from "@azure/communication-common";
+import {
+  ChatComposite,
+  MessageProps,
+  MessageRenderer,
+  useAzureCommunicationChatAdapter,
+  fromFlatCommunicationIdentifier,
+} from "@azure/communication-react";
 import { AdaptiveCard } from "adaptivecards-react";
+import { Action } from "adaptivecards";
 
 interface UserObject {
-  userId: CommunicationUserIdentifier,
-  credential: AzureCommunicationTokenCredential,
+  userId: CommunicationUserIdentifier;
+  credential: AzureCommunicationTokenCredential;
 }
-
 function App() {
-
   const [user, setUser] = useState<UserObject>();
-  const [threadId, setThreadId] = useState();
-  const endpoint = process.env.REACT_APP_ENDPOINT; //Replace with your endpoint
+  const [threadId, setThreadId] = useState("");
+  const endpoint = ""; //Replace with your endpoint
 
   useEffect(() => {
-    if(process.env.REACT_APP_FUNCTION_URL) {
-      fetch(process.env.REACT_APP_FUNCTION_URL)
-      .then(response => response.json())
-      .then(data => {
-        setUser({
-          userId: data.user, 
-          credential: new AzureCommunicationTokenCredential(data.userToken.token)
-        })
-        setThreadId(data.threadId)
-      });
-    }
-  },[])
+    // Response returned by Chat Function
+    const tmpConf: any = {
+      user: {
+        communicationUserId: "", // Replace with a user id for the user in a chat thread with adaptive cards bot
+      },
+      userToken: {
+        token: "", // Replace with ACS token
+        expiresOn: "", // Can be skipped, the field is returned in Chat Function response
+      },
+      threadId: "", // Replace with a thread id with the user and the bot
+    };
+
+    setUser({
+      userId: fromFlatCommunicationIdentifier(
+        tmpConf.user.communicationUserId
+      ) as CommunicationUserIdentifier,
+      credential: new AzureCommunicationTokenCredential(
+        tmpConf.userToken.token
+      ),
+    });
+    setThreadId(tmpConf.threadId);
+  }, []);
 
   const credential = useMemo(() => {
     try {
       return user?.credential;
     } catch {
-      console.error('Failed to construct token credential');
+      console.error("Failed to construct token credential");
       return undefined;
     }
   }, [user]);
-
-  const userId = useMemo(() => {
-    try {
-      return user?.userId;
-    } catch {
-      console.error('Failed to construct token credential');
-      return undefined;
-    }
-  }, [user])
 
   const adapter = useAzureCommunicationChatAdapter({
     endpoint,
     userId: user?.userId,
     displayName: "Local User",
     credential,
-    threadId
+    threadId,
   });
 
-  const onRenderMessage = (messageProps: MessageProps, defaultOnRender?: MessageRenderer): JSX.Element => {
+  const onRenderMessage = (
+    messageProps: MessageProps,
+    defaultOnRender?: MessageRenderer
+  ): JSX.Element => {
+    if (
+      messageProps.message.messageType === "chat" &&
+      // messageProps.message.metadata &&
+      messageProps.message.content
+    ) {
+      try {
+        // for (const [key, value] of Object.entries(
+        //   messageProps.message.metadata
+        // )) {
+        // check if message is sent form a bot
+        // if (value === "azurebotservice.adaptivecard") {
+        let hostConfig = {
+          fontFamily: "Times New Roman",
+        };
 
-    if (messageProps.message.messageType === 'chat' && messageProps.message.metadata && messageProps.message.content) {
-      for (const [key, value] of Object.entries(messageProps.message.metadata))
-      {
-        if(value === "azurebotservice.adaptivecard") {
-          let hostConfig = {
-            fontFamily: "Segoe UI, Helvetica Neue, sans-serif"
-          };
-          let card = JSON.parse(messageProps.message.content).attachments[0].content
-          return <AdaptiveCard payload={card} hostConfig={hostConfig} />
-        }
+        let adaptiveCard = JSON.parse(messageProps.message.content);
+        let cardContent = adaptiveCard.attachments[0].content;
+
+        return (
+          <AdaptiveCard
+            payload={cardContent}
+            hostConfig={hostConfig}
+            onExecuteAction={(action: Action) =>
+              console.log("onExecuteAction " + action)
+            }
+            onActionOpenUrl={(action: Action) =>
+              console.log("onActionOpenUrl" + action)
+            }
+            onActionShowCard={(action: Action) =>
+              console.log("onActionShowCard" + action)
+            }
+            onError={(error: any) => console.log("onError", error)}
+          />
+        );
+        // }
+        // }
+      } catch (error) {
+        console.log("try/catch error", messageProps.message.content);
+        return defaultOnRender ? defaultOnRender(messageProps) : <></>;
       }
     }
 
@@ -76,11 +115,8 @@ function App() {
 
   if (adapter) {
     return (
-      <div style={{ height: '90vh', width: '90vw' }}>
-        <ChatComposite
-          adapter={adapter}
-          onRenderMessage={onRenderMessage}
-        />
+      <div style={{ height: "90vh", width: "90vw" }}>
+        <ChatComposite adapter={adapter} onRenderMessage={onRenderMessage} />
       </div>
     );
   }
--- ChatApp/src/index.tsx
+++ ChatApp/src/index.tsx
@@ -1,14 +1,21 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import * as Adaptive from "adaptivecards";
+import ProgressBar from "./ProgressBar";
+
+// Added registration for a custom element ProgressBar
+Adaptive.GlobalRegistry.elements.register(
+  ProgressBar.JsonTypeName,
+  ProgressBar
+);
 
 ReactDOM.render(
   <div className="wrapper">
     <App />
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
